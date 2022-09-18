cf1daf3459701129d6fa77975d51064c7471533b
Martinus Werts <martinus.werts@ens-rennes.fr>
2022-09-18 22:11:48 +0200

    safename fixing UNPRINTABLE, WHITESPACE and BADCHARS problems. prototype.

--- safename.py
+++ safename.py
@@ -37,8 +37,9 @@ def checkcheck(root, fdn, dftype, fixit = False):
     """
     Check for compatibility problems of an entry in the directory tree.
     
-    TODO: also fix problems, but ONLY FIX ONE PROBLEM AT A TIME!!!!
-    Problem resolution order should be WHITESPACE, BADCHARS, SYMLINK
+    Can also fix these problems. FIXES ONLY ONE PROBLEM AT A TIME! 
+    Else problems are expected. Use multiple runs to fix files displaying
+    several problems at the same time (e.g. SYMLINK with BADCHARS)
 
     Parameters
     ----------
@@ -59,18 +60,25 @@ def checkcheck(root, fdn, dftype, fixit = False):
     fixed1problem = False
     pp = Path(root, fdn)
 
+    #
+    # 1. UNPRINTABLE check
+    #
     # Check for presence of character with ASCII/Unicode code below 32
     # (unprintable characters). This is a dangerous problem and should be 
-    # dealt with manually before using `safename`
+    # dealt with manually before using `safename`.
+    # Thus, we will raise an AssertionError and crash the program.
+    # (unless we LIVEDANGEROUSLY = True)
+    
     below32 = True in [ord(c)<32 for c in fdn]
     if not LIVEDANGEROUSLY:
         assert not below32, "FATAL: illegal character in "+str(pp)
     else:
-        # live dangerously
+        # living dangerously
         if below32:
             print('UNPRINTABLE\t'+str(pp)+'\t'+dftype)
             if fixit and not fixed1problem:
                 fixed1problem = True
+                # create new name, simply skipping all unprintable characters
                 goodname = ''
                 for c in fdn:
                     if ord(c)<32:
@@ -89,30 +97,63 @@ def checkcheck(root, fdn, dftype, fixit = False):
                     print('FIX\tTARGET NAME ALREADY EXISTS')
                 else:
                     print('FIX\tTarget name does not exist. Go ahead.')
+                    pp.rename(goodpp)
                 print('FIX\t**************')
-                pp.rename(goodpp)
-   
-    if fdn.strip() != fdn:
+     
+    #
+    # 2. WHITESPACE check
+    #
+    goodname = fdn.strip()
+    if goodname != fdn:
         print('WHITESPACE\t'+str(pp)+'\t'+dftype)
         if fixit and not fixed1problem:
             fixed1problem = True
-            print('FIX\t***FIX WHITESPACE*** TO DO...')
+            goodpp = Path(root, goodname)
+            print('FIX\t*************')
+            print('FIX\t*** bad name:')
+            print('FIX\t',str(pp))
+            print('FIX\t*************')
+            print('FIX\t*** good name:')
+            print('FIX\t',str(goodpp))
+            print('FIX\t*************')
+            if goodpp.exists():
+                print('FIX\tTARGET NAME ALREADY EXISTS')
+            else:
+                print('FIX\tTarget name does not exist. Go ahead.')
+                pp.rename(goodpp)
+            print('FIX\t**************')
 
+    #
+    # 3. BADCHARS check
+    # 
     # backslash, use r'string'
-    #TODO: change algo -> create corrected version then compare with original
-    # use .translate()
-    containsForbidden = True in [c in fdn for c in r'<>:"/\|?*']
-    if containsForbidden:
+    fixchars = str.maketrans(r'<>:"/\|?*', r"[]-'____x")
+    goodname = fdn.translate(fixchars)
+    if goodname != fdn:
         print('BADCHARS\t'+str(pp)+'\t'+dftype)
         if fixit and not fixed1problem:
             fixed1problem = True
-            print('FIX\t***FIX BADCHARS*** TO DO...')
+            goodpp = Path(root, goodname)
+            print('FIX\t*************')
+            print('FIX\t*** bad name:')
+            print('FIX\t',str(pp))
+            print('FIX\t*************')
+            print('FIX\t*** good name:')
+            print('FIX\t',str(goodpp))
+            print('FIX\t*************')
+            if goodpp.exists():
+                print('FIX\tTARGET NAME ALREADY EXISTS')
+            else:
+                print('FIX\tTarget name does not exist. Go ahead.')
+                pp.rename(goodpp)
+            print('FIX\t**************')
 
     if pp.is_symlink():
         print('SYMLINK\t'+str(pp)+'\t'+dftype)
         if fixit and not fixed1problem:
             fixed1problem = True
             print('FIX\t***FIX SYMLINK*** TO DO...')
+        
         
 #%% main code
 
