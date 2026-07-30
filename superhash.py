91a359070b42ec6d95241f3f75398f81bf39d18b
Martinus Werts <martinus.werts@univ-angers.fr>
2026-07-30 22:45:08 +0200

    superhash 0.2dev, beginning support for restarting interrupted scans

--- superhash.py
+++ superhash.py
@@ -5,7 +5,7 @@
 #         to explicitly specify 'utf-8' as the encoding of files, else
 #         Python will use the platform-specific encoding
 
-__version__ = '0.1a3'  
+__version__ = '0.2'  
 
 CHUNKSIZE = 67108864 # 64 MiB size, for hashing in chunks
 
@@ -17,6 +17,7 @@ from pathlib import Path
 import hashlib
 from datetime import datetime
 import csv
+import json
 
 from tqdm import tqdm
 
@@ -31,7 +32,7 @@ clargs = cli.parse_args()
 
 print('')
 print("This is MANBAMM's superhash - v"+__version__+\
-      " - by M.H.V. Werts, 2022-2025")
+      " - by M.H.V. Werts, 2022-2026")
 print("")
 
 dtn = datetime.now()
@@ -67,14 +68,72 @@ with open(p_result, 'w', encoding='utf-8') as fout:
     writer.writerow(['# superhash-start-timestamp-iso', dtn.isoformat()])
     writer.writerow(['# absolute-path-source-dir',p_src_abs.as_posix()])
     writer.writerow(['# absolute-path-superhash-file',p_result_abs.as_posix()])
+
+    #TODO: restart mechanics (see below in the 'walklist' part) will probably
+    #      go here, at least in part
+    
+    # Cold start ()
+    print('Preparing file list... please stand by...')
+    
+    # sorts according to root, keeping the subdirs and files in sync
+    walklist = sorted(list(os.walk(p_src_abs)))
+    
+    # insert walklist as JSONL block into file to enable the restart of an 
+    # aborted superhash scan
+    
+    #TODO: implement the mechanics for such a restart! (insert just above)
+    #   simply add `--restart <filename>`
+    #   which should create a new file, 
+    #   initially copying the restart file and the continue where it left off
+    
+    fout.write("#\n")
+    fout.write("#BEGIN-WALKLIST-JSONL\n")
+    for dirpath, dirnames, filenames in walklist:
+        line = json.dumps([dirpath, dirnames, filenames], ensure_ascii=False)
+        fout.write("#" + line + "\n")
+    fout.write("#END-WALKLIST-JSONL\n")
+    fout.write("#\n")
+ 
+# The walklist may be reconstructed from the 'restart file'
+#  along the following line
+# 
+#    walklist = []
+#    
+#    line = f.readline()
+#    assert line.strip() == "#BEGIN-WALKLIST-JSONL" # or perhaps a softer mechanism
+#
+#    for line in f:
+#        stripped = line.rstrip("\n")
+#        if stripped == "#END-WALKLIST-JSONL":
+#            break
+#        assert stripped.startswith("#"), "malformed walklist line"
+#        dirpath, dirnames, filenames = json.loads(stripped[1:])
+#        walklist.append((dirpath, dirnames, filenames))
+#
+#    print(walklist) # or do something better with the walklist
+#
+# and the walklist is back (first test, perhaps, compare to original)
+    
+ 
+    print('Done! Moving on...')
+    
+    # start of actual TSV block
     writer.writerow(['# timestamp_iso',
                      'rel_path_posix',
                      'filename',
                      'mtime_iso',
                      'size',
                      'md5digest'])
-    # sorts according to root, keeping the subdirs and files in sync
-    walklist = sorted(list(os.walk(p_src_abs)))
+    
+    #TODO: restart mechanics - at this point we may have a produced a reduced 
+    #     walklist 
+    #  that only
+    #      contains the part of the walklist still to be processed
+    #  perhaps anticipate that some of the files in the remaining walklist
+    #   may have disappeared and can be simply skipped (with warning?)
+    #         => print final report when script finishes (files in walklist
+    #            vs files in hash list)
+    
     for root, subdirs, files in tqdm(walklist):
         checksums = []
         rootrelative = os.path.relpath(root, p_src_abs.parent)
