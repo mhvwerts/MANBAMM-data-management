4851e24f15c99c7ecf9a3b31d437b2000acc06d0
Martinus Werts <martinus.werts@univ-angers.fr>
2026-08-02 00:10:29 +0200

    superhash 0.2, resume mode working (experimental)

--- superhash.py
+++ superhash.py
@@ -22,12 +22,14 @@ import json
 from tqdm import tqdm
 
 cli = argparse.ArgumentParser()
-cli.add_argument("src_dir", type=str,
-                 help="source directory to be scanned")
 cli.add_argument('-n', '--nohash', action='store_true',
                  help='do not calculate hashes, only generate file info tree')
 cli.add_argument("-o", "--outpath", type=str,
                  help="path or pathname of result file")
+cli.add_argument("-r", "--resume", type=str,
+                 help="resume superhash based on existing file")
+cli.add_argument("-s", "--src_dir", type=str,
+                 help="source directory to be scanned")
 clargs = cli.parse_args()
 
 print('')
@@ -37,103 +39,161 @@ print("")
 
 dtn = datetime.now()
 
-p_src = Path(clargs.src_dir)
-if not p_src.is_dir():
-    sys.exit("Specified source is not a directory.")
-p_src_abs = p_src.resolve(strict=True)
+if clargs.resume is not None:
+    #
+    # restart from existing, incomplete superhash data file
+    #
+    p_result = Path(clargs.resume)
+    p_result_abs = p_result.resolve(strict=False)
+   
+    with open(p_result, 'r', encoding='utf-8') as fin:
+        rdr = csv.reader(fin, delimiter='\t', quoting=csv.QUOTE_NONE)
+        header = [rdr.__next__() for i in range(4)]
+        if not (header[0][0] == '# superhash-version'):
+            print(f'Error: not a superhash file "{clargs.resume}"')
+            sys.exit(2)
+        if not (header[0][1] == __version__):
+            print('Error: File generated with a different version of superhash. Revise your script.', 
+                  file=sys.stderr)
+            print('       File generated with v'+header[0][1]+', current software v'+__version__,
+                  file=sys.stderr)
+            sys.exit(2)
+        
+        # This header line contains the absolute path as a posix string
+        # Readily converted back to an actual path
+        p_src_abs = Path(header[2][1])
 
-md5st = 'noMD5' if clargs.nohash else ''
-dts = dtn.strftime('%y%m%d_%H%M%S')
-result_file = p_src_abs.stem+"_sh"+dts+md5st+".tsv"
-if clargs.outpath is None:
-    p_result = Path(result_file)
+        walklist = []
+        
+        line = fin.readline() # this is an empty '#' comment line
+        line = fin.readline()
+        assert line.strip() == "#BEGIN-WALKLIST-JSONL", "Ill-formatted input file. Cannot resume."
+         
+        for line in fin:
+            stripped = line.rstrip("\n")
+            if stripped == "#END-WALKLIST-JSONL":
+                break
+            assert stripped.startswith("#"), "malformed walklist line"
+            dirpath, dirnames, filenames = json.loads(stripped[1:])
+            walklist.append((dirpath, dirnames, filenames))
+            
+        line = fin.readline() # this is an empty '#' comment line
+        line = fin.readline()
+        assert line.strip() == "#BEGIN-SUPERHASH-TSV", "Ill-formatted input file. Cannot resume."
+        line = fin.readline() # skip TSV header line
+        
+       
+        print('Scanning for resume point...')
+
+        # Here we use a cheap scanning mechanism: just compare file-names
+        # This is not 100% without risk, but probably OK.
+        # Worst case: we skip/rescan some files and may have problems
+        # resuming again after a new interruption
+        #TODO: give this some extra scrutiny
+        # and perhaps add a check of the path (last few members)
+        
+        walkix = 0 # scan over walklist by index
+        tsvendfound = False
+        for root, subdirs, files in tqdm(walklist):
+            fileix=0
+            for file in tqdm(sorted(files), leave = False):
+                try:
+                    rawtsvln = rdr.__next__()
+                except StopIteration:
+                    tsvendfound = True
+                    break
+                if not file==rawtsvln[2]:
+                    print(f'Error: incompatible file names: "{file}" "{rawtsvln[2]}" ')
+                    sys.exit(2)
+                fileix+=1
+            if tsvendfound:
+                break                    
+            walkix+=1
+
+        # remove files already processed from current walklist line
+        walklist[walkix] = (walklist[walkix][0],
+                            walklist[walkix][1],
+                            walklist[walkix][2][fileix:])
+        # keep only remaining files from walklist
+        walklist = walklist[walkix:]
+
 else:
-    p_out = Path(clargs.outpath)
-    if p_out.is_dir():
-        p_result = Path(p_out, result_file)
-    else:
-        result_file = clargs.outpath
+    #
+    # Start afresh. Create a fresh file, with a fresh header and a fresh
+    # walklist.
+    # 
+    if clargs.src_dir is None:
+        print("Error: Please supply a --src_dir", file=sys.stderr)
+        sys.exit(2)
+    p_src = Path(clargs.src_dir)
+    if not p_src.is_dir():
+        print("Error: Specified source is not a directory", file=sys.stderr)
+        sys.exit(2)
+    p_src_abs = p_src.resolve(strict=True)
+    
+    md5st = 'noMD5' if clargs.nohash else ''
+    dts = dtn.strftime('%y%m%d_%H%M%S')
+    result_file = p_src_abs.stem+"_sh"+dts+md5st+".tsv"
+    if clargs.outpath is None:
         p_result = Path(result_file)
+    else:
+        p_out = Path(clargs.outpath)
+        if p_out.is_dir():
+            p_result = Path(p_out, result_file)
+        else:
+            result_file = clargs.outpath
+            p_result = Path(result_file)
+    
+    p_result_abs = p_result.resolve(strict=False)
+    
+    print('Source directory:   ', str(p_src))
+    print('Output file     :   ', str(p_result))
+    print('')
 
-p_result_abs = p_result.resolve(strict=False)
+    with open(p_result, 'w', encoding='utf-8') as fout:
+        writer = csv.writer(fout, delimiter='\t', lineterminator='\n',
+                            quoting=csv.QUOTE_NONE)
+        writer.writerow(['# superhash-version', __version__])
+        writer.writerow(['# superhash-start-timestamp-iso', dtn.isoformat()])
+        writer.writerow(['# absolute-path-source-dir',p_src_abs.as_posix()])
+        writer.writerow(['# absolute-path-superhash-file',p_result_abs.as_posix()])
 
-print('Source directory:   ', str(p_src))
-print('Output file     :   ', str(p_result))
-print('')
+        
+        # Cold start 
+        print('Preparing file list... please stand by...')
+        
+        # sorts according to root, keeping the subdirs and files in sync
+        walklist = sorted(list(os.walk(p_src_abs)))
+        
+        # insert walklist as JSONL block into file to enable the restart of an 
+        # aborted superhash scan
+        fout.write("#\n")
+        fout.write("#BEGIN-WALKLIST-JSONL\n")
+        for dirpath, dirnames, filenames in walklist:
+            line = json.dumps([dirpath, dirnames, filenames], ensure_ascii=False)
+            fout.write("#" + line + "\n")
+        fout.write("#END-WALKLIST-JSONL\n")
+        fout.write("#\n")
+         
+        # start of actual TSV block
+        fout.write("#BEGIN-SUPERHASH-TSV\n")
+        writer.writerow(['# timestamp_iso',
+                         'rel_path_posix',
+                         'filename',
+                         'mtime_iso',
+                         'size',
+                         'md5digest'])
 
-with open(p_result, 'w', encoding='utf-8') as fout:
+print()
+print('Done! Moving on...')
+print()
+
+
+# Reopen file in APPEND mode to write the TSV superhash lines
+#  do not forget to re-instantiate the CSV writer
+with open(p_result, 'a', encoding='utf-8') as fout:
     writer = csv.writer(fout, delimiter='\t', lineterminator='\n',
                         quoting=csv.QUOTE_NONE)
-    writer.writerow(['# superhash-version', __version__])
-    writer.writerow(['# superhash-start-timestamp-iso', dtn.isoformat()])
-    writer.writerow(['# absolute-path-source-dir',p_src_abs.as_posix()])
-    writer.writerow(['# absolute-path-superhash-file',p_result_abs.as_posix()])
-
-    #TODO: restart mechanics (see below in the 'walklist' part) will probably
-    #      go here, at least in part
-    
-    # Cold start ()
-    print('Preparing file list... please stand by...')
-    
-    # sorts according to root, keeping the subdirs and files in sync
-    walklist = sorted(list(os.walk(p_src_abs)))
-    
-    # insert walklist as JSONL block into file to enable the restart of an 
-    # aborted superhash scan
-    
-    #TODO: implement the mechanics for such a restart! (insert just above)
-    #   simply add `--restart <filename>`
-    #   which should create a new file, 
-    #   initially copying the restart file and the continue where it left off
-    
-    fout.write("#\n")
-    fout.write("#BEGIN-WALKLIST-JSONL\n")
-    for dirpath, dirnames, filenames in walklist:
-        line = json.dumps([dirpath, dirnames, filenames], ensure_ascii=False)
-        fout.write("#" + line + "\n")
-    fout.write("#END-WALKLIST-JSONL\n")
-    fout.write("#\n")
- 
-# The walklist may be reconstructed from the 'restart file'
-#  along the following lines
-# 
-#    walklist = []
-#    
-#    line = f.readline()
-#    assert line.strip() == "#BEGIN-WALKLIST-JSONL" # or perhaps a softer mechanism
-#
-#    for line in f:
-#        stripped = line.rstrip("\n")
-#        if stripped == "#END-WALKLIST-JSONL":
-#            break
-#        assert stripped.startswith("#"), "malformed walklist line"
-#        dirpath, dirnames, filenames = json.loads(stripped[1:])
-#        walklist.append((dirpath, dirnames, filenames))
-#
-#    print(walklist) # or do something better with the walklist
-#
-# and the walklist is back (first test, perhaps, compare to original)
-    
- 
-    print('Done! Moving on...')
-    
-    # start of actual TSV block
-    writer.writerow(['# timestamp_iso',
-                     'rel_path_posix',
-                     'filename',
-                     'mtime_iso',
-                     'size',
-                     'md5digest'])
-    
-    #TODO: restart mechanics - at this point we may have a produced a reduced 
-    #     walklist 
-    #  that only
-    #      contains the part of the walklist still to be processed
-    #  perhaps anticipate that some of the files in the remaining walklist
-    #   may have disappeared and can be simply skipped (with warning?)
-    #         => print final report when script finishes (files in walklist
-    #            vs files in hash list)
-    
     for root, subdirs, files in tqdm(walklist):
         checksums = []
         rootrelative = os.path.relpath(root, p_src_abs.parent)
@@ -143,21 +203,35 @@ with open(p_result, 'w', encoding='utf-8') as fout:
         for file in tqdm(sorted(files), leave = False):
             filepath = Path(root, file)
             if (filepath.resolve() == p_result_abs):
-                tqdm.write('... skipping result file itself ('\
-                           +str(p_result)+')')
+                # tqdm.write('... skipping result file itself ('\
+                #            +str(p_result)+')')
+                print("Error: The result file must not be inside the scanned directory tree.", file=sys.stderr)
+                print("Tip:   Try renaming the result file in-place and restart. It will not be in the walklist anymore.", file=sys.stderr)
+                sys.exit(2)
             else:
-                fpstat = filepath.stat()
-                fpsize = fpstat.st_size
-                mtime_iso = datetime.fromtimestamp(fpstat.st_mtime).isoformat()
-                with open(filepath, 'rb') as _file:
-                    if clargs.nohash:
-                        md5digest = ''
-                    else:
-                        cumhash = hashlib.md5()
-                        for chunk in iter(lambda: _file.read(CHUNKSIZE), b''):
-                            cumhash.update(chunk)
-                        md5digest = cumhash.hexdigest()
                 timestamp_iso = datetime.now().isoformat()
+                # Files might be gone between creation of walklist and actual
+                # scan. Not a problem (if limited to a few files)
+                #TODO: emit warning and/or set limit
+                if filepath.exists():
+                    fpstat = filepath.stat()
+                    fpsize = fpstat.st_size
+                    mtime_iso = datetime.fromtimestamp(fpstat.st_mtime).isoformat()
+                    with open(filepath, 'rb') as _file:
+                        if clargs.nohash:
+                            md5digest = ''
+                        else:
+                            cumhash = hashlib.md5()
+                            for chunk in iter(lambda: _file.read(CHUNKSIZE), b''):
+                                cumhash.update(chunk)
+                            md5digest = cumhash.hexdigest()
+                else:
+                    # insert place holder info
+                    # keeping the file list in sync
+                    mtime_iso = timestamp_iso 
+                    fpsize = 0
+                    md5digest = '!FILE_GONE'
+
                 checksums.append([timestamp_iso,
                                   rootrel_posix,
                                   file,
@@ -165,5 +239,13 @@ with open(p_result, 'w', encoding='utf-8') as fout:
                                   fpsize,
                                   md5digest])
         writer.writerows(checksums)
+        
+    # Write end marker. The presence of this marker indicates that the
+    # full tree was scanned and included in the superhash file.
+    # If the marker is absent (v0.2 file format), this signifies that
+    # the superhash data in the file is incomplete.
+    #TODO: include this in 'superhash-check.py'
+    fout.write("#END-SUPERHASH-TSV\n")
+    fout.write("#\n")
 print('')
 print('')
